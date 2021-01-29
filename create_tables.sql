5c57e217c1615019b0e26877665f7aa90e5eb1ee
brandon <daughertyjam@gmail.com>
2021-01-29 16:03:53 -0500

    Added list all machines to JDBC

--- create_tables.sql
+++ create_tables.sql
@@ -3,14 +3,15 @@ CREATE TABLE machine (
 	brand VARCHAR NOT NULL,
 	model VARCHAR NOT NULL,
 	tonnage INTEGER NOT NULL,
-	tie_bar_horizontal TIMESTAMP NOT NULL,
+	tie_bar_horizontal INTEGER NOT NULL,
         tie_bar_vertical INTEGER NOT NULL,
         platten_horizontal INTEGER NOT NULL,
         platten_vertical INTEGER NOT NULL,
         mold_height_min INTEGER NOT NULL,
         mold_height_max INTEGER NOT NULL,
         shot_size_pe INTEGER NOT NULL,
-        shot_size_ps INTEGER NOT NULL
+        shot_size_ps INTEGER NOT NULL,
+        date_added TIMESTAMP
 );
 
 CREATE TABLE tool (
@@ -31,4 +32,12 @@ CREATE TABLE assignments (
         constraint fk_assignment_tool foreign key (tool_id) references tool(tool_id),
         constraint fk_assignment_machine foreign key (machine_id) references machine(machine_id)
 );
+
+INSERT INTO machine (machine_id, brand, model, tonnage, tie_bar_horizontal, tie_bar_vertical, platten_horizontal, platten_vertical, mold_height_min, mold_height_max, shot_size_pe, shot_size_ps, date_added) 
+VALUES(18, 'Shibaura Machine', 'EC55SXIII', 55, 410, 360, 510, 460, 150, 370, 63, 50, CURRENT_TIMESTAMP);
+
+INSERT INTO machine (machine_id, brand, model, tonnage, tie_bar_horizontal, tie_bar_vertical, platten_horizontal, platten_vertical, mold_height_min, mold_height_max, shot_size_pe, shot_size_ps, date_added) 
+VALUES(19, 'MILACRON', 'Roboshot', 55, 360, 320, 500, 470, 210, 410, 57, 46, CURRENT_TIMESTAMP);
+
+
 
--- src/main/java/com/brandonendeavors/dao/MachineSqlDAO.java
+++ src/main/java/com/brandonendeavors/dao/MachineSqlDAO.java
@@ -1,9 +1,49 @@
 package com.brandonendeavors.dao;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
 
+import com.brandonendeavors.model.Machine;
+
+@Component
 public class MachineSqlDAO implements MachineDAO {
 
-//Pass in a Tool and return a list of 3 machines.
+	private JdbcTemplate jdbcTemplate;
+
+	public MachineSqlDAO(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public List<Machine> listAllMachines() {
+		List<Machine> machines = new ArrayList<Machine>();
+		String sql = "select * from machine";
+
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+		while (results.next()) {
+			Machine machine = mapRowToMachine(results);
+			machines.add(machine);
+		}
+
+		return machines;
+	}
 
 
+	
+	
+	
+	
+	
+	
+	private Machine mapRowToMachine(SqlRowSet rs) {
+		Machine machine = new Machine();
+		machine.setMachineId(rs.getInt("machine_id"));
+		machine.setUsername(rs.getString("username"));
+		machine.setPassword(rs.getString("password_hash"));
+		machine.setActivated(true);
+		return machine;
+	}
 }
