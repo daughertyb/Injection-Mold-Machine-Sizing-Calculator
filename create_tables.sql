5945d9bbea1baece8be027e43ed8163c9519f836
brandon <daughertyjam@gmail.com>
2021-01-29 14:04:34 -0500

    Database Script and Minor JDBC Updates

--- create_tables.sql
+++ create_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE machine (
 	machine_id integer UNIQUE PRIMARY KEY,
-	brand VARCHAR ( 50 ) NOT NULL,
-	model VARCHAR ( 50 ) NOT NULL,
+	brand VARCHAR NOT NULL,
+	model VARCHAR NOT NULL,
 	tonnage INTEGER NOT NULL,
 	tie_bar_horizontal TIMESTAMP NOT NULL,
         tie_bar_vertical INTEGER NOT NULL,
@@ -13,9 +13,22 @@ CREATE TABLE machine (
         shot_size_ps INTEGER NOT NULL
 );
 
+CREATE TABLE tool (
+        tool_id INTEGER UNIQUE PRIMARY KEY,
+        client_name VARCHAR,
+        width INTEGER NOT NULL,
+        height INTEGER NOT NULL,
+        stack INTEGER NOT NULL,
+        shot_size INTEGER NOT NULL
+);
+
 CREATE TABLE assignments (
         quote_id serial PRIMARY KEY,
-        tool_number serial,
+        client_name VARCHAR,
         machine_id INTEGER,
-        date_assigned TIMESTAMP
+        tool_id INTEGER,
+        date_assigned TIMESTAMP,
+        constraint fk_assignment_tool foreign key (tool_id) references tool(tool_id),
+        constraint fk_assignment_machine foreign key (machine_id) references machine(machine_id)
 );
+
--- src/main/java/com/brandonendeavors/dao/MachineSqlDAO.java
+++ src/main/java/com/brandonendeavors/dao/MachineSqlDAO.java
@@ -3,4 +3,7 @@ package com.brandonendeavors.dao;
 
 public class MachineSqlDAO implements MachineDAO {
 
+//Pass in a Tool and return a list of 3 machines.
+
+
 }
