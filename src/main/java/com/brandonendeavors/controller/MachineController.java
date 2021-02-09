0c6a15f0d1829a98f5a17ab4265d44aaa10feec9
brandon <daughertyjam@gmail.com>
2021-02-08 21:51:42 -0500

    updating dependencies

--- src/main/java/com/brandonendeavors/controller/MachineController.java
+++ src/main/java/com/brandonendeavors/controller/MachineController.java
@@ -3,13 +3,16 @@ package com.brandonendeavors.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.brandonendeavors.dao.MachineDAO;
 import com.brandonendeavors.model.Machine;
 
-
+@CrossOrigin
+@RestController
 public class MachineController {
 
 	@Autowired
